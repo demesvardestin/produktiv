d7eb133472a5935e313320b32d1f465b9f4606c6
demo07 <dddemesvar07@gmail.com>
2019-01-14 01:37:45 +0000

    commit

--- git_job.sh
+++ git_job.sh
@@ -3,17 +3,19 @@
 git_job() {
   case "$1" in
     "add")
+    
     echo $2 >> commits.txt
     echo "$2 added to commit list"
     return 0
     ;;
+    
     "schedule")
+    
     shift
-    git_job sha remotename remotebranchname timeinterval
-    SEND_IN=`$4*3600`
+    SEND_IN=`expr $4 * 3600`
     sleep $SEND_IN
     
-    git rebase -i
+    git rebase $3
     if [ "$1" == "no-sha" ]; then
         shift
         git push $1 $2 $SEND_IN
@@ -22,6 +24,7 @@ git_job() {
     SHA="$1:$3"
     git push $2 $SHA
     return 0
+    
     ;;
   esac
 }
